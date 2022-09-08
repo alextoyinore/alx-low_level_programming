59cd5b5dd73eeadbf10d00735273de49c3e722c0
Alexander Ore <aore8030@gmail.com>
2022-09-08 16:43:20 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success) 
+ */
+
+int main()
+{
+   char Mystr[] = "Programming is like building a multilingual puzzle"
+   puts(Mystr); //writing the sting to sddout
+   return (0) 
+}
