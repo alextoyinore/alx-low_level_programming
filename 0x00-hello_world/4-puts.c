2d493d5ea4f64d898dead58f3beb815630e30c9c
Alexander Ore <aore8030@gmail.com>
2022-09-08 16:45:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main()
 {
-   char Mystr[] = "Programming is like building a multilingual puzzle"
-   puts(Mystr); //writing the sting to sddout
-   return (0) 
+   char Mystr[] = "Programming is like building a multilingual puzzle";
+   puts(Mystr);
+   return (0); 
 }
