c3520324ca03292ccaf10479fa090849e0af78af
Alexander Ore <aore8030@gmail.com>
2022-09-09 21:35:26 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 97;
+
+	while(n < 123)
+	{
+		putchar(n);
+		n = n + 1;
+	}
+
+	return (0);
+}
+
