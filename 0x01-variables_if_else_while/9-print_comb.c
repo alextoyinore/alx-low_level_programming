ee8b869342763c70d123f7cde90e62e6c32609ce
Alexander Ore <aore8030@gmail.com>
2022-09-09 22:42:25 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 48;
+
+	while (n < 58)
+	{
+		putchar(n);
+		putchar(44);
+		putchar(32);
+		n = n + 1;
+	}
+
+	putchar(10);
+
+	return (0);
+}
+
