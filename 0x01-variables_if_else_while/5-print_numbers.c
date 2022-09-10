7e2e47ee6e0cbb5b89ee9287613947592706f8b2
Alexander Ore <aore8030@gmail.com>
2022-09-09 22:11:06 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		print(n);
+		n = n + 1;
+	}
+
+	putchar(10);
+
+	return (0);
+}
+
