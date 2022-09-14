28df84dd1c73745dc31b8dfe8bfc7eb79104524b
Alexander Ore <aore8030@gmail.com>
2022-09-14 14:07:05 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,22 @@
-extern int make_iso_compilers_happy;
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *str = "_putchar";
+
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
