9100e44d16aafd966824748fd487835c354634b0
Alexander Ore <aore8030@gmail.com>
2022-09-14 11:56:31 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the lowercase letters of the english alphabet
+ *
+ * Return: void 
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for(i='a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
