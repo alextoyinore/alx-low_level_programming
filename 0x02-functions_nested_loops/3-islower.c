ca8f3ed1e926da0f929c30f3fd2d6e0717a45c7c
Alexander Ore <aore8030@gmail.com>
2022-09-14 16:38:35 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - check is a character is lowercase or not
+ * @c: value to be passed in
+ *
+ * Return: 1 if true and 0 if not
+ */
+
+int _islower(int c)
+{
+	if(c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int r;
+	r = _islower('H');
+	_putchar(r + '0');
+	return (0);
+}
