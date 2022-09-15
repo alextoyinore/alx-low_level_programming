96528a7a72df3a147891396cfb582ccf7fc2e761
Alexander Ore <aore8030@gmail.com>
2022-09-15 16:53:57 -0700

    6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - multiplies two digits
+ * @n: argument passed
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if ((n = 0) || (n = '\n')
+		{
+			_putchar('\n');		
+		}
+		else
+		{
+			_putchar('_' + '0');
+		}
+	}
+
+	_putchar('\n');
+}
