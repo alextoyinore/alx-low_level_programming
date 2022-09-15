a97c09e1426e17502048c8cce2fff12935fed8d1
Alexander Ore <aore8030@gmail.com>
2022-09-14 18:01:55 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Prints the absolute value of an integer
+ *
+ * Return: int
+ */
+
+int _abs(int n)
+{
+	int n;
+
+	if (n < 0)
+	{
+		return (-1 * n);
+	}
+
+	return (n);
+}
