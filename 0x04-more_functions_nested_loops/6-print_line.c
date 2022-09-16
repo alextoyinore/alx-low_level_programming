5757ee3566151243348ac36023ee54ed7d417311
Alexander Ore <aore8030@gmail.com>
2022-09-16 02:38:44 -0700

    6-print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,15 @@
 
 void print_line(int n)
 {
-	int i;
-
-	for (i = 0; i <= n; i++)
+	if (n <= 0)
 	{
-		if ((n == 0) || (n < 0))
-		{
-			_putchar('\n');		
-		}
-		else
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_' + '0');
 		}
