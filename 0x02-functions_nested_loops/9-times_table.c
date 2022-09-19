453eca7a6c0f9f805a11055a71e9be13753086db
Alexander Ore <aore8030@gmail.com>
2022-09-18 22:36:29 -0700

    catching up with neated loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,28 +15,26 @@ void times_table(void)
 	while (i <= 9)
 	{
 
-		j = 0;
+		j = 1;
 
 		while (j <= 9)
 		{
 			result = i * j;
 
-			if ((result / 10) > 0)
+			if ((result <= 9)
 			{
 				_putchar((result / 10) + '0');
 			}
 			else
 			{
-				_putchar(' ');
+				_putchar((result / 10) + '0');
 			}
 
 			_putchar((result % 10) + '0');
 
-			if (j < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+
+			_putchar(',');
+			_putchar(' ');
 			j++;
 		}
 
