c844e543304b93e598ce05babcc0c482e896336d
Alexander Ore <aore8030@gmail.com>
2022-09-20 02:57:24 -0700

    fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	for (; src[i] != '\0'; i++)
+	if (src != NULL)
 	{
-		dest[i] = src[i];
+	
+		for (; src[i] != '\0'; i++)
+			dest[i] = src[i];
+		}
 	}
 	return (dest);
 }
