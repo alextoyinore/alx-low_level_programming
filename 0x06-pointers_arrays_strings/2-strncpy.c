aa27e1d8f7c52a12c609328c9056965cee8c9a86
Alexander Ore <aore8030@gmail.com>
2022-09-22 14:33:45 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,20 @@
  * Return: dest
  */
 
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
+	i = 0;
+
+	while (src[i] != '\0')
+	{
 		if (i < n)
 			dest[i] = src[i];
 		while (i < n)
 			dest[i++] = '\0';
+	}
 
 	return (dest);
 }
+
