5a29fea38eed4fb479061d04d49a26f0feb1ca74
Alexander Ore <aore8030@gmail.com>
2022-09-22 14:59:43 -0700

    rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses a given array
+ * @a: array parameter
+ * n: number of elements in the array
+ *
+ * Return: Always 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if ((n - i) == n)
+			continue;
+
+		_putchar(a[n-i])
+	}
+
+	return (0);
+}
